87cace7ab38650f10f8ff1e5ef98d02107a04843
gitForDavidL <71913304+gitForDavidL@users.noreply.github.com>
2021-04-11 07:53:13 -0500

    check

---
+++ src/co/edu/unbosque/model/Lista.java
@@ -0,0 +1,45 @@
+package co.edu.unbosque.model;
+
+public class Lista {
+
+    private Nodo primero;
+
+    public Lista() {
+        primero = null;
+    }
+
+    public boolean listaVacia() {
+        return primero == null;
+    }
+
+    public void insertar(int elemento, int peso) {
+        if (listaVacia()) {
+            Nodo nuevo = new Nodo(elemento, peso);
+            nuevo.setSiguiente(primero);
+            primero = nuevo;
+        } else {
+            Nodo actual = primero;
+            while (actual.getSiguiente() != null) {
+                actual = actual.getSiguiente();
+            }
+            Nodo nuevo = new Nodo(elemento, peso);
+            actual.setSiguiente(nuevo);
+        }
+    }
+
+    public String toString() {
+        if (!listaVacia()) {
+            Nodo actual = primero;
+            String r = "";
+            while (actual != null) {
+                r += actual.toString() + " -> ";
+                actual = actual.getSiguiente();
+            }
+            r = r.substring(0, r.length() - 3);
+            return r;
+        } else {
+            return "Lista vacia";
+        }
+    }
+
+}
